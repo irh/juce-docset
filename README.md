1989492fb86f8c025e500238fc377448c619359f
irh <ian.r.hobson@gmail.com>
2017-02-05 14:58:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# juce-docset
+
+## About
+
+A docset generator script for the [JUCE](https://github.com/julianstorer/JUCE)
+library.
+
+## Dependencies
+
+- Doxygen
+- ImageMagick
+
+## Usage
+
+- Run `./make-docset.sh`.
+- The resulting docset will appear in `JUCE.docset`.
---
+++ make-docset.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git submodule update --init
+
+cd juce/doxygen
+sed -i.bak \
+  -e 's/.*GENERATE_DOCSET.*= NO.*/GENERATE_DOCSET = YES/' \
+  -e 's/.*DISABLE_INDEX.*= NO.*/DISABLE_INDEX = YES/' \
+  -e 's/.*SEARCHENGINE.*= YES.*/SEARCHENGINE = NO/' \
+  -e 's/.*GENERATE_TREEVIEW.*= YES.*/GENERATE_TREEVIEW = NO/' \
+  Doxyfile
+doxygen
+cd doc
+make
+cp -r org.doxygen.Project.docset ../../../JUCE.docset
+cd ../../..
+
+iconPath=JUCE/extras/Projucer/Source/BinaryData/juce_icon.png
+convert $iconPath -resize 16x16 JUCE.docset/Icon.png
+convert $iconPath -resize 32x32 JUCE.docset/Icon@2x.png
